2eeeaef9628ae2f919be40cf36da4af13b1db1b0
Jordyn Newnham <jordyn@husk.pro>
2022-03-15 19:07:55 +0800

    feat: support setting custom images (#1)

--- README.md
+++ README.md
@@ -7,30 +7,33 @@ This project was made a lot easier referencing the work done by [liquidctl](http
 ## Example usage
 
 ```rust
+use std::path::Path;
+
 use color_eyre::Result;
+use rusb::{open_device_with_vid_pid, set_log_level};
 use zkraken_lib::{NZXTDevice, PID, VID};
 
 fn main() -> Result<()> {
-    let api = hidapi::HidApi::new()?;
-    let device = api.open(VID, PID)?;
-
-    let mut nzxt_device = NZXTDevice {
-        device: &device,
-        initialised: false,
-    };
-
-    nzxt_device.initialise()?;
+    #[cfg(debug_assertions)]
+    set_log_level(rusb::LogLevel::Debug);
+    
+    // We need to use RUSB as well because HIDAPI doesn't support the writing to BULK endpoint.
+    let mut handle = open_device_with_vid_pid(VID, PID).expect("No Kraken Z device found!");
+    let api = hidapi_rusb::HidApi::new()?;
+    let hid_device = api.open(VID, PID)?;
+    let nzxt_device = NZXTDevice::new(&hid_device, &mut handle, 270)?;
 
     let firmware = nzxt_device.get_firmware_version()?;
     println!("Firmware version: {}", firmware);
 
-    nzxt_device.set_brightness(100)?;
+    let status = nzxt_device.get_status()?;
+    println!("Status: {:?}", status);
+
     nzxt_device.set_fan_duty(80)?;
     nzxt_device.set_pump_duty(80)?;
 
-    let status = nzxt_device.get_status()?;
-
-    println!("Status: {:?}", status);
+    let image = Path::new("duck.jpg");
+    nzxt_device.set_image(image, 1, true)?;
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
+use std::path::Path;
+use std::time::Duration;
+
 use color_eyre::eyre::eyre;
 use color_eyre::eyre::Result;
-use hidapi::HidDevice;
+use hidapi_rusb::HidDevice;
+use image::GenericImageView;
+use rusb::DeviceHandle;
+use rusb::UsbContext;
 
 pub const PUMP_ENDPOINT_ADDRESS: u8 = 0x1;
 pub const FAN_ENDPOINT_ADDRESS: u8 = 0x2;
-
-pub const WRITE_ENDPOINT_ADDRESS: u8 = 0x1;
-pub const BULK_WRITE_ENDPOINT_ADDRESS: u8 = 0x2;
-pub const READ_ENDPOINT_ADDRESS: u8 = 0x81;
 
 pub const WRITE_LENGTH: usize = 64;
 pub const BULK_WRITE_LENGTH: usize = 512;
@@ -15,13 +17,16 @@ pub const BULK_WRITE_LENGTH: usize = 512;
 pub const READ_LENGTH: usize = 64;
 pub const BULK_READ_LENGTH: usize = 512;
 
+pub const BULK_TIMEOUT: Duration = std::time::Duration::from_secs(10);
+
 // Kraken Z series.
 pub const VID: u16 = 0x1e71;
 pub const PID: u16 = 0x3008;
 
-pub struct NZXTDevice<'a> {
+pub struct NZXTDevice<'a, T: UsbContext> {
     pub device: &'a HidDevice,
-    pub initialised: bool,
+    pub bulk_endpoint_handle: &'a mut DeviceHandle<T>,
+    pub rotation_degrees: i32,
 }
 
 #[derive(Debug)]
@@ -33,14 +38,29 @@ pub struct DeviceStatus {
     pub fan_duty: i32,
 }
 
-impl NZXTDevice<'_> {
-    /// Check if the device is initialised.
-    fn check_if_initalised(&self) -> Result<()> {
-        if !self.initialised {
-            return Err(eyre!("NZXTDevice not initialised."));
-        }
+impl<T: UsbContext> NZXTDevice<'_, T> {
+    /// Create an instance of NZXTDevice.
+    pub fn new<'a>(
+        device: &'a HidDevice,
+        bulk_endpoint_handle: &'a mut DeviceHandle<T>,
+        rotation_degrees: i32,
+    ) -> Result<NZXTDevice<'a, T>> {
+        // Set auto detach kernel driver.
+        bulk_endpoint_handle.set_auto_detach_kernel_driver(true)?;
 
-        Ok(())
+        // Claim the interface that the BULK endpoint is on.
+        bulk_endpoint_handle.claim_interface(0)?;
+
+        let mut nzxt_device = NZXTDevice {
+            device,
+            bulk_endpoint_handle,
+            rotation_degrees,
+        };
+
+        // Init the device.
+        nzxt_device.initialise()?;
+
+        Ok(nzxt_device)
     }
 
     /// Initialise the NZXT device.
@@ -50,23 +70,35 @@ impl NZXTDevice<'_> {
         // We read here to throw away the response bytes.
         self.read()?;
 
-        self.initialised = true;
+        Ok(())
+    }
+
+    /// Write INTERRUPT raw bytes to the NZXT device.
+    fn write(&self, data: &[u8]) -> Result<()> {
+        let mut buf = [0u8; WRITE_LENGTH];
+        buf.fill(0x0);
+
+        // Copy the data to a new buffer with the correct length.
+        if data.len() > WRITE_LENGTH {
+            buf.copy_from_slice(data);
+        }
+
+        // Write to the USB device via the endpoint.
+        self.device.write(data)?;
 
         Ok(())
     }
 
-    /// Write 64 bytes (WRITE_LENGTH) to the device.
-    fn write(&self, data: &[u8]) -> Result<()> {
-        let mut buffer = [0u8; WRITE_LENGTH];
+    /// Write BULK raw bytes to the NZXT device.
+    fn write_bulk(&self, data: &[u8]) -> Result<()> {
+        let mut buf = [0u8; BULK_WRITE_LENGTH];
+        buf.fill(0x0);
 
-        // Copy the data into the start of the buffer.
-        buffer[0..data.len()].copy_from_slice(data);
-
-        // Pad the remainder of the buffer with null bytes.
-        buffer[data.len()..].fill(0x0);
+        buf.copy_from_slice(data);
 
         // Write to the USB device via the endpoint.
-        self.device.write(&buffer)?;
+        self.bulk_endpoint_handle
+            .write_bulk(0x02, &buf, BULK_TIMEOUT)?;
 
         Ok(())
     }
@@ -91,7 +123,6 @@ impl NZXTDevice<'_> {
 
     /// Return the status of the device.
     pub fn get_status(&self) -> Result<DeviceStatus> {
-        self.check_if_initalised()?;
         self.clear_read_buffer()?;
 
         self.write(&[0x74, 0x01])?;
@@ -127,8 +158,6 @@ impl NZXTDevice<'_> {
 
     /// Set the given duty for the specified endpoint.
     fn set_duty(&self, duty: u8, address: u8) -> Result<()> {
-        self.check_if_initalised()?;
-
         if (20..=100).contains(&duty) {
             let mut buffer = [0u8; WRITE_LENGTH];
 
@@ -155,8 +184,6 @@ impl NZXTDevice<'_> {
 
     /// Get the firmware version from the device.
     pub fn get_firmware_version(&self) -> Result<String> {
-        self.check_if_initalised()?;
-
         // Request firmware info.
         self.write(&[0x10, 0x01])?;
 
@@ -169,8 +196,6 @@ impl NZXTDevice<'_> {
 
     /// Set the visual mode for the device.
     pub fn set_visual_mode(&self, mode: u8, index: u8) -> Result<()> {
-        self.check_if_initalised()?;
-
         let mut buffer = [0u8; WRITE_LENGTH];
 
         buffer.fill(0x0);
@@ -179,6 +204,21 @@ impl NZXTDevice<'_> {
         buffer[1] = 0x01;
         buffer[2] = mode;
         buffer[3] = index;
+
+        self.write(&buffer)?;
+
+        Ok(())
+    }
+
+    /// Clear the memory bucket at given index.
+    pub fn clear_bucket(&self, index: u8) -> Result<()> {
+        let mut buffer = [0u8; WRITE_LENGTH];
+
+        buffer.fill(0x0);
+
+        buffer[0] = 0x38;
+        buffer[1] = 0x02;
+        buffer[2] = index;
 
         self.write(&buffer)?;
 
@@ -202,8 +242,6 @@ impl NZXTDevice<'_> {
 
     /// Set the device LCD brightness.
     pub fn set_brightness(&self, brightness: u8) -> Result<()> {
-        self.check_if_initalised()?;
-
         if (0..=100).contains(&brightness) {
             let mut buffer = [0u8; WRITE_LENGTH];
             buffer.fill(0x0);
@@ -225,6 +263,154 @@ impl NZXTDevice<'_> {
         }
 
         Err(eyre!("Duty value is out of bounds"))
+    }
+
+    pub fn set_image(
+        &self,
+        path_to_image: &Path,
+        index: u8,
+        apply_image_after_upload: bool,
+    ) -> Result<()> {
+        let mut img = image::open(path_to_image)?;
+
+        let (width, height) = img.dimensions();
+
+        if self.rotation_degrees == 90 {
+            img = img.rotate90();
+        } else if self.rotation_degrees == 180 {
+            img = img.rotate180();
+        } else if self.rotation_degrees == 270 {
+            img = img.rotate270();
+        }
+
+        if width != 320 && height != 320 {
+            img = img.resize_exact(320, 320, image::imageops::FilterType::Gaussian);
+        }
+
+        let image_bytes = img.to_rgba8().into_raw();
+        let image_size_bytes = image_bytes.len() as i32;
+
+        self.upload_image(
+            &image_bytes,
+            image_size_bytes,
+            index,
+            apply_image_after_upload,
+        )?;
+
+        Ok(())
+    }
+
+    fn upload_image(
+        &self,
+        image_bytes: &[u8],
+        image_size_bytes: i32,
+        index: u8,
+        apply_image_after_upload: bool,
+    ) -> Result<()> {
+        self.set_visual_mode(1, index)?;
+        self.clear_bucket(index)?;
+
+        let memory_slot = index as u16 * 800;
+        let memory_slot_count = (image_size_bytes / 1024) as u16;
+
+        self.setup_bucket(index, index + 1, memory_slot, memory_slot_count)?;
+        self.write_start_bucket(index)?;
+        self.send_bulk_data_info(2)?;
+
+        // Time to write the image bytes!
+        self.bulk_endpoint_handle
+            .write_bulk(0x02, image_bytes, BULK_TIMEOUT)?;
+
+        self.write_finish_bucket(index)?;
+
+        if apply_image_after_upload {
+            self.set_visual_mode(4, index)?;
+        }
+
+        Ok(())
+    }
+
+    /// Set up bucket for uploading an image.
+    pub fn setup_bucket(
+        &self,
+        index: u8,
+        id: u8,
+        memory_slot: u16,
+        memory_slot_count: u16,
+    ) -> Result<()> {
+        let mut buffer = [0u8; WRITE_LENGTH];
+
+        buffer.fill(0x0);
+
+        buffer[0] = 0x32;
+        buffer[1] = 0x01;
+        buffer[2] = index;
+        buffer[3] = id;
+        buffer[4] = (memory_slot >> 8) as u8;
+        buffer[5] = memory_slot as u8;
+        buffer[6] = memory_slot_count as u8;
+        buffer[7] = (memory_slot_count >> 8) as u8;
+        buffer[8] = 1;
+
+        self.write(&buffer)?;
+
+        Ok(())
+    }
+
+    /// Send the write start to given bucket.
+    pub fn write_start_bucket(&self, index: u8) -> Result<()> {
+        let mut buffer = [0u8; WRITE_LENGTH];
+
+        buffer.fill(0x0);
+
+        buffer[0] = 0x36;
+        buffer[1] = 0x1;
+        buffer[2] = index;
+
+        self.write(&buffer)?;
+
+        Ok(())
+    }
+
+    pub fn write_finish_bucket(&self, index: u8) -> Result<()> {
+        let mut buffer = [0u8; WRITE_LENGTH];
+
+        buffer.fill(0x0);
+
+        buffer[0] = 0x36;
+        buffer[1] = 0x2;
+        buffer[2] = index;
+
+        self.write(&buffer)?;
+
+        Ok(())
+    }
+
+    pub fn send_bulk_data_info(&self, mode: u8) -> Result<()> {
+        let mut buffer = [0u8; BULK_WRITE_LENGTH];
+        buffer.fill(0x0);
+
+        // Fill with 12fa01e8abcdef987654321 (magic numbers) and then mode.
+        buffer[0] = 0x12;
+        buffer[1] = 0xfa;
+        buffer[2] = 0x01;
+        buffer[3] = 0xe8;
+        buffer[4] = 0xab;
+        buffer[5] = 0xcd;
+        buffer[6] = 0xef;
+        buffer[7] = 0x98;
+        buffer[8] = 0x76;
+        buffer[9] = 0x54;
+        buffer[10] = 0x32;
+        buffer[11] = 0x10;
+        buffer[12] = mode;
+        // More magic?
+        buffer[17] = 0x40;
+        buffer[18] = 0x96;
+
+        self.write_bulk(&buffer)?;
+
+        Ok(())
     }
 }
 
